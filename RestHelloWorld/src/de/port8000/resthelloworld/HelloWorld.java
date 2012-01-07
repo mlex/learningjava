8af6626989b3c4c7b04ac7c824dd49ab31d9c6ee
Michael Lex <m.lex@port8000.de>
2012-01-08 00:40:50 +0100

    added variable stored in session (even if this is not clean rest anymore)

--- RestHelloWorld/src/de/port8000/resthelloworld/HelloWorld.java
+++ RestHelloWorld/src/de/port8000/resthelloworld/HelloWorld.java
@@ -1,19 +1,23 @@
 package de.port8000.resthelloworld;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloWorld {
+	
 	// This method is called if TEXT_PLAIN is request
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayPlainTextHello() {
-		return "Hello Jersey!";
+		return "Hello Jersey";
 	}
-
+	
 	// This method is called if XML is request
 	@GET
 	@Produces(MediaType.TEXT_XML)
@@ -24,8 +28,21 @@ public class HelloWorld {
 	// This method is called if HTML is request
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
+	public String sayHtmlHello(@Context HttpServletRequest req) {
+		
+
+        HttpSession session= req.getSession(true);
+        Object foo = session.getAttribute("foo");
+        String add;
+        if (foo!=null) {
+        	session.setAttribute("foo", foo.toString() + "!");
+        	add = foo.toString();
+        } else {
+            session.setAttribute("foo", "!");
+            add = "";
+        }
+
 		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+				+ "<body><h1>" + "Hello Jersey" + add + "</body></h1>" + "</html> ";
 	}
 }
